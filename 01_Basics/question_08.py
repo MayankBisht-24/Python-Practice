38c22a9eed94f644bf00158addd996f825456aa1
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-10 20:02:10 +0530

    Add question 08: Print personal information

---
+++ 01_Basics/question_08.py
@@ -0,0 +1,26 @@
+"""
+Question 08
+
+Title:
+Print Personal Information
+
+Problem Statement:
+Write a Python program to print the following information.
+
+Expected Output:
+Name: Mayank Bisht
+City: Haridwar
+Course: BCA Data Science
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String Formatting (Basic)
+"""
+
+# Solution starts here
+print("Name: Mayank Bisht")
+print("City: Haridwar")
+print("Course: BCA Data Science")
