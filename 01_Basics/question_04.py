a1016eb1b853541982368173a82f6375d9a939df
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-10 19:55:03 +0530

    Add question 04: Print your city

---
+++ 01_Basics/question_04.py
@@ -0,0 +1,22 @@
+"""
+Question 04
+
+Title:
+Print Your City
+
+Problem Statement:
+Write a Python program to print the name of your city.
+
+Expected Output:
+Haridwar
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String
+"""
+
+# Solution starts here
+print("Haridwar")
