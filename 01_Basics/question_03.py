6d7682a44dbd1db0f9f972d2058d1338511d9b97
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-10 19:43:32 +0530

    Add question 03: Print your name

---
+++ 01_Basics/question_03.py
@@ -0,0 +1,23 @@
+"""
+Question 03
+
+Title:
+Print Your Name
+
+Problem Statement:
+Write a Python program to print your full name.
+
+Expected Output:
+Mayank Bisht
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String
+"""
+
+# Solution starts here
+
+print("Mayank Bisht")
