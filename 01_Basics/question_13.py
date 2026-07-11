7ae2eef33d4978ca30727986854953907cde803f
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-11 21:32:29 +0530

    Add question 13: Print a triangle

---
+++ 01_Basics/question_13.py
@@ -0,0 +1,31 @@
+"""
+Question 13
+
+Title:
+Print a Triangle
+
+Problem Statement:
+Write a Python program to print the following pattern.
+
+Expected Output:
+*
+**
+***
+****
+*****
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Pattern
+"""
+
+# Solution starts here
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
