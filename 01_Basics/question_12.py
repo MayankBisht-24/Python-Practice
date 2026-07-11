63823eea03f0d7bf3ffb2d5d02e42a42564c6a17
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-11 21:31:59 +0530

    Add question 12: Print symbols

---
+++ 01_Basics/question_12.py
@@ -0,0 +1,30 @@
+"""
+Question 12
+
+Title:
+Print Symbols
+
+Problem Statement:
+Write a Python program to print the following symbols.
+
+Expected Output:
+@
+#
+$
+%
+&
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+"""
+
+# Solution starts here
+
+print("@")
+print("#")
+print("$")
+print("%")
+print("&")
