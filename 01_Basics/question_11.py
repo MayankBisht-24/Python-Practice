c71b6ae99b8e9dfe79042d4eb7a44d3b816b7bca
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-11 21:31:10 +0530

    Add question 11: Print using escape characters

---
+++ 01_Basics/question_11.py
@@ -0,0 +1,25 @@
+"""
+Question 11
+
+Title:
+Print Using Escape Characters
+
+Problem Statement:
+Write a Python program to print the following output.
+
+Expected Output:
+Python
+    is
+        Awesome
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Escape Characters
+"""
+
+# Solution starts here
+
+print("Python\n\tis\n\t\tAwesome")
