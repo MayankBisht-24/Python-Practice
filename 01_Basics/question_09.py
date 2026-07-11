b4af330918d0d6d865392a0b8676f9efaf97c2e0
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-11 21:28:43 +0530

    Add question 09: Print a rectangle pattern

---
+++ 01_Basics/question_09.py
@@ -0,0 +1,29 @@
+"""
+Question 09
+
+Title:
+Print a Rectangle Pattern
+
+Problem Statement:
+Write a Python program to print the following rectangle pattern.
+
+Expected Output:
+*****
+*****
+*****
+*****
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Pattern
+"""
+
+# Solution starts here
+
+print("*****")
+print("*****")
+print("*****")
+print("*****")
