b55c556f0442c973d6564cd53a744e7c16839129
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-11 21:30:44 +0530

    Add question 10: Print personal details

---
+++ 01_Basics/question_10.py
@@ -0,0 +1,27 @@
+"""
+Question 10
+
+Title:
+Print Personal Details
+
+Problem Statement:
+Write a Python program to print your name, age and country.
+
+Expected Output:
+Name: Mayank Bisht
+Age: 23
+Country: India
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String
+"""
+
+# Solution starts here
+
+print("Name: Mayank Bisht")
+print("Age: 23")
+print("Country: India")
