cd6b7f663b4f0a8a4cb6f305bbca964af0ab5f2e
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:52:30 +0530

    Add question 15: Print a countdown

---
+++ 01_Basics/question_15.py
@@ -0,0 +1,31 @@
+"""
+Question 15
+
+Title:
+Print a Countdown
+
+Problem Statement:
+Write a Python program to print a countdown from 5 to 1.
+
+Expected Output:
+5
+4
+3
+2
+1
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Integer
+"""
+
+# Solution starts here
+
+print(5)
+print(4)
+print(3)
+print(2)
+print(1)
