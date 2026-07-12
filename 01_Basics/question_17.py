4ee3b8503a12e3dc68e93970c2b25cd9b102533c
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:53:20 +0530

    Add question 17: Print a box

---
+++ 01_Basics/question_17.py
@@ -0,0 +1,29 @@
+"""
+Question 17
+
+Title:
+Print a Box
+
+Problem Statement:
+Write a Python program to print the following box.
+
+Expected Output:
++-------+
+|       |
+|       |
++-------+
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Pattern
+"""
+
+# Solution starts here
+
+print("+-------+")
+print("|       |")
+print("|       |")
+print("+-------+")
