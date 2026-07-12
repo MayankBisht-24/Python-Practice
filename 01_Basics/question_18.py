10e87bba202fe6a0a665550126c0abab08d1aeef
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:53:49 +0530

    Add question 18: Print university information

---
+++ 01_Basics/question_18.py
@@ -0,0 +1,27 @@
+"""
+Question 18
+
+Title:
+Print School Information
+
+Problem Statement:
+Write a Python program to print the following information.
+
+Expected Output:
+University : Amity University Online
+Course     : BCA
+Specialization : Data Science
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String Formatting
+"""
+
+# Solution starts here
+
+print("University : Amity University Online")
+print("Course     : BCA")
+print("Specialization : Data Science")
