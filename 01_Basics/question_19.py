2c1f0f465b67ddc271c2d9e294d7bce5089bcfb0
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:54:08 +0530

    Add question 19: Print a right triangle pattern

---
+++ 01_Basics/question_19.py
@@ -0,0 +1,31 @@
+"""
+Question 19
+
+Title:
+Print a Right Triangle Pattern
+
+Problem Statement:
+Write a Python program to print the following pattern.
+
+Expected Output:
+#
+##
+###
+####
+#####
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- Pattern
+"""
+
+# Solution starts here
+
+print("#")
+print("##")
+print("###")
+print("####")
+print("#####")
