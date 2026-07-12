5c274f54f5333df6b15589b50e69326596186385
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:54:28 +0530

    Add question 20: Complete Python basics

---
+++ 01_Basics/question_20.py
@@ -0,0 +1,25 @@
+"""
+Question 20
+
+Title:
+Complete Python Basics
+
+Problem Statement:
+Write a Python program to print the following message.
+
+Expected Output:
+Congratulations!
+You have successfully completed 20 Python Basics questions.
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String
+"""
+
+# Solution starts here
+
+print("Congratulations!")
+print("You have successfully completed 20 Python Basics questions.")
