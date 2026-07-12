8579de3d6f6f304eea782ef0a0cd90ef2c643e6c
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-12 21:52:52 +0530

    Add question 16: Print your favourite programming language

---
+++ 01_Basics/question_16.py
@@ -0,0 +1,23 @@
+"""
+Question 16
+
+Title:
+Print Your Favourite Programming Language
+
+Problem Statement:
+Write a Python program to print your favourite programming language.
+
+Expected Output:
+Python
+
+Difficulty:
+Beginner
+
+Concepts:
+- print()
+- String
+"""
+
+# Solution starts here
+
+print("Python")
