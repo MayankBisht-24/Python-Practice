5aca05973a830fb96512a6acd51fc55753d948f5
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-13 21:24:18 +0530

    Add operators question 02: Subtract two numbers

---
+++ 02_Operators/question_02.py
@@ -0,0 +1,32 @@
+"""
+Question 02
+
+Title:
+Subtract Two Numbers
+
+Problem Statement:
+Write a Python program to take two numbers as input and print their difference.
+
+Expected Output:
+Enter first number: 25
+Enter second number: 10
+Difference = 15
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Subtraction Operator (-)
+"""
+
+# Solution starts here
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+difference = num1 - num2
+
+print("Difference =", difference)
