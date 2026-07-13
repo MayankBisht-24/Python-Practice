02fd986811766cde5f47c27e3e2220254d1efd0f
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-13 21:18:28 +0530

    Add operators question 01: Add two numbers

---
+++ 02_Operators/question_01.py
@@ -0,0 +1,32 @@
+"""
+Question 01
+
+Title:
+Add Two Numbers
+
+Problem Statement:
+Write a Python program to take two numbers as input and print their sum.
+
+Expected Output:
+Enter first number: 10
+Enter second number: 20
+Sum = 30
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Addition Operator (+)
+"""
+
+# Solution starts here
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_result = num1 + num2
+
+print("Sum =", sum_result)
