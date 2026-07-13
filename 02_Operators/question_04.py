ae99fef36800ce15b54716f6bfdd6a7ead5a037f
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-13 21:25:55 +0530

    Add operators question 04: Divide two numbers

---
+++ 02_Operators/question_04.py
@@ -0,0 +1,32 @@
+"""
+Question 04
+
+Title:
+Divide Two Numbers
+
+Problem Statement:
+Write a Python program to take two numbers as input and print their quotient.
+
+Expected Output:
+Enter first number: 20
+Enter second number: 4
+Quotient = 5.0
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Division Operator (/)
+"""
+
+# Solution starts here
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+quotient = num1 / num2
+
+print("Quotient =", quotient)
