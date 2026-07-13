69167199393b6f7602bbd786f4951f648f9167c5
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-13 21:24:50 +0530

    Add operators question 03: Multiply two numbers

---
+++ 02_Operators/question_03.py
@@ -0,0 +1,32 @@
+"""
+Question 03
+
+Title:
+Multiply Two Numbers
+
+Problem Statement:
+Write a Python program to take two numbers as input and print their product.
+
+Expected Output:
+Enter first number: 8
+Enter second number: 6
+Product = 48
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Multiplication Operator (*)
+"""
+
+# Solution starts here
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+product = num1 * num2
+
+print("Product =", product)
