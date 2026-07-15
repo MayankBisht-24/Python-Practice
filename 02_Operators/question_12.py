01f09a9ef449ca42613a777a236de75b3083edae
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-15 19:39:09 +0530

    Add operators question 12: Square of a number

---
+++ 02_Operators/question_12.py
@@ -0,0 +1,30 @@
+"""
+Question 12
+
+Title:
+Square of a Number
+
+Problem Statement:
+Write a Python program to calculate the square of a number.
+
+Expected Output:
+Enter a number: 9
+Square = 81
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Exponent Operator (**)
+"""
+
+# Solution starts here
+
+number = int(input("Enter a number: "))
+
+square = number ** 2
+
+print("Square =", square)
