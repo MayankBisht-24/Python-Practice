880652d9208b891868045d42e6c4c8b447cc9563
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-15 19:39:36 +0530

    Add operators question 13: Cube of a number

---
+++ 02_Operators/question_13.py
@@ -0,0 +1,30 @@
+"""
+Question 13
+
+Title:
+Cube of a Number
+
+Problem Statement:
+Write a Python program to calculate the cube of a number.
+
+Expected Output:
+Enter a number: 4
+Cube = 64
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Exponent Operator (**)
+"""
+
+# Solution starts here
+
+number = int(input("Enter a number: "))
+
+cube = number ** 3
+
+print("Cube =", cube)
