5f0c0ed9453c71f0bbc15db8ca1041b0768698b0
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-16 21:10:22 +0530

    Add operators question 16: Area of rectangle

---
+++ 02_Operators/question_16.py
@@ -0,0 +1,32 @@
+"""
+Question 16
+
+Title:
+Calculate Area of a Rectangle
+
+Problem Statement:
+Write a Python program to calculate the area of a rectangle.
+
+Expected Output:
+Enter length: 12
+Enter width: 5
+Area = 60
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- int()
+- Multiplication Operator (*)
+"""
+
+# Solution starts here
+
+length = int(input("Enter length: "))
+width = int(input("Enter width: "))
+
+area = length * width
+
+print("Area =", area)
