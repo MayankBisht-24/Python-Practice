f7ae8e45c924ce1b504d5c90eb508de0c7ed7b90
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-16 21:10:54 +0530

    Add operators question 17: Area of circle

---
+++ 02_Operators/question_17.py
@@ -0,0 +1,30 @@
+"""
+Question 17
+
+Title:
+Calculate Area of a Circle
+
+Problem Statement:
+Write a Python program to calculate the area of a circle.
+
+Expected Output:
+Enter radius: 7
+Area = 153.94
+
+Difficulty:
+Beginner
+
+Concepts:
+- Variables
+- input()
+- float()
+- Exponent Operator (**)
+"""
+
+# Solution starts here
+
+radius = float(input("Enter radius: "))
+
+area = 3.14 * radius ** 2
+
+print("Area =", area)
