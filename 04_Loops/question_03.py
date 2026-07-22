41dd633d44f74b23750f57c76dccb0b0749c2c53
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-22 19:50:48 +0530

    Add loops question 03: Print even numbers from 1 to 20

---
+++ 04_Loops/question_03.py
@@ -0,0 +1,33 @@
+"""
+Question 03
+
+Title:
+Print Even Numbers from 1 to 20
+
+Problem Statement:
+Write a Python program to print all even numbers from 1 to 20.
+
+Expected Output:
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+
+Difficulty:
+Beginner
+
+Concepts:
+- for loop
+- range()
+"""
+
+# Solution starts here
+
+for number in range(2, 21, 2):
+    print(number)
