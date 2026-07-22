1d131864bcb7a840b2c0475e9a40828f4b64e40b
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-22 19:50:20 +0530

    Add loops question 02: Print numbers from 10 to 1

---
+++ 04_Loops/question_02.py
@@ -0,0 +1,33 @@
+"""
+Question 02
+
+Title:
+Print Numbers from 10 to 1
+
+Problem Statement:
+Write a Python program to print numbers from 10 to 1.
+
+Expected Output:
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+Difficulty:
+Beginner
+
+Concepts:
+- for loop
+- range()
+"""
+
+# Solution starts here
+
+for number in range(10, 0, -1):
+    print(number)
