cb92be2f83b62cb7db8efd6631ae983939537ea7
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-22 19:51:11 +0530

    Add loops question 04: Print odd numbers from 1 to 20

---
+++ 04_Loops/question_04.py
@@ -0,0 +1,33 @@
+"""
+Question 04
+
+Title:
+Print Odd Numbers from 1 to 20
+
+Problem Statement:
+Write a Python program to print all odd numbers from 1 to 20.
+
+Expected Output:
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
+Difficulty:
+Beginner
+
+Concepts:
+- for loop
+- range()
+"""
+
+# Solution starts here
+
+for number in range(1, 20, 2):
+    print(number)
