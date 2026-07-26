39e45302ce7cae1ecb597712d0a4406683110bb1
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-26 21:10:39 +0530

    Add functions question 01: Create first function

---
+++ 05_Functions/question_01.py
@@ -0,0 +1,29 @@
+"""
+Question 01
+
+Title:
+Create Your First Function
+
+Problem Statement:
+Write a Python function named greet() that prints
+"Welcome to Python Functions!".
+
+Call the function once.
+
+Expected Output:
+Welcome to Python Functions!
+
+Difficulty:
+Beginner
+
+Concepts:
+- def
+- Function Calling
+"""
+
+# Solution starts here
+
+def greet():
+    print("Welcome to Python Functions!")
+
+greet()
