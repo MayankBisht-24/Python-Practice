d705bff297f8dcd96f67acbc054a7b60a1d109d0
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-26 21:12:32 +0530

    Add functions question 02: Display name

---
+++ 05_Functions/question_02.py
@@ -0,0 +1,28 @@
+"""
+Question 02
+
+Title:
+Function to Print Your Name
+
+Problem Statement:
+Write a function named display_name() that prints your name.
+
+Call the function.
+
+Expected Output:
+My name is Mayank.
+
+Difficulty:
+Beginner
+
+Concepts:
+- Function Definition
+- Function Calling
+"""
+
+# Solution starts here
+
+def display_name():
+    print("My name is Mayank.")
+
+display_name()
