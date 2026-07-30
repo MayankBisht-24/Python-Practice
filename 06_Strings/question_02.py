6b503179eb56645dd734e9a6f0adc03b60591d98
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-30 21:15:20 +0530

    Add strings question 02: Find string length

---
+++ 06_Strings/question_02.py
@@ -0,0 +1,26 @@
+"""
+Question 02
+
+Title:
+Find the Length of a String
+
+Problem Statement:
+Write a Python program to find the length of a string.
+
+Expected Output:
+Enter a string: Python
+
+Length = 6
+
+Difficulty:
+Beginner
+
+Concepts:
+- len()
+"""
+
+# Solution starts here
+
+text = input("Enter a string: ")
+
+print("Length =", len(text))
