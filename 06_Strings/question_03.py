95a61a1a69f318fd7d03fb253c59f5a7816d1ceb
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-30 21:16:41 +0530

    Add strings question 03: Convert to uppercase

---
+++ 06_Strings/question_03.py
@@ -0,0 +1,26 @@
+"""
+Question 03
+
+Title:
+Convert String to Uppercase
+
+Problem Statement:
+Write a Python program to convert a string to uppercase.
+
+Expected Output:
+Enter a string: python
+
+Uppercase = PYTHON
+
+Difficulty:
+Beginner
+
+Concepts:
+- upper()
+"""
+
+# Solution starts here
+
+text = input("Enter a string: ")
+
+print("Uppercase =", text.upper())
