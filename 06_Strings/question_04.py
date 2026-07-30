1fcd33f3db359b34cfc3ba2c0e27eca0f8b23013
Mayank Bisht <mayanksinghbisht4248@gmail.com>
2026-07-30 21:17:07 +0530

    Add strings question 04: Convert to lowercase

---
+++ 06_Strings/question_04.py
@@ -0,0 +1,26 @@
+"""
+Question 04
+
+Title:
+Convert String to Lowercase
+
+Problem Statement:
+Write a Python program to convert a string to lowercase.
+
+Expected Output:
+Enter a string: PYTHON
+
+Lowercase = python
+
+Difficulty:
+Beginner
+
+Concepts:
+- lower()
+"""
+
+# Solution starts here
+
+text = input("Enter a string: ")
+
+print("Lowercase =", text.lower())
